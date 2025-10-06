0e004e3796e24d4e1694d5f4f1894ab79e324f19
Luan Souza <alunoluansenai@gmail.com>
2025-10-06 11:13:47 -0300

    ✨ Novo Recurso: Recipes templates e arquivos static

--- recipes/views.py
+++ recipes/views.py
@@ -3,4 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 
 def Home(request):
-    return render(request, "recipes/pages/home.html")
+
+    recipes = range(1, 5)
+
+    return render(request, "recipes/pages/home.html", context={
+        "numeros": recipes,
+        "is_detail_page": False,
+    })
+
+
+def Recipes_Detalhe(request, id):
+    
+    return render(request, "recipes/pages/recipes_detalhe.html", context={
+        "numeros" : "1",
+        "is_detail_page": True,
+    })
